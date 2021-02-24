32dc0a1a0627a3e0c05d5256e62410cadcf8702e
Saowaluck Suksawat <poppy@odds.team>
2021-02-24 17:08:49 +0700

    Initail docker airflow

---
+++ dags/pop.py
@@ -0,0 +1,26 @@
+import datetime
+
+from airflow.models import DAG
+from airflow.operators.bash import BashOperator
+
+
+dag = DAG(
+    dag_id='popppppppp',
+    default_args={'start_date': datetime.datetime(2017, 5, 5)},
+    schedule_interval='*/5 * * * *',
+    catchup=False,
+)
+
+t1 = BashOperator(
+    task_id='first_task',
+    bash_command='echo "first task"',
+    dag=dag
+)
+
+t2 = BashOperator(
+    task_id='second_task',
+    bash_command='echo "second task"',
+    dag=dag
+)
+
+t1 >> t2
---
+++ readme.md
@@ -0,0 +1,15 @@
+## How to install airflow
+
+- mkdir ./dags ./logs ./plugins
+echo -e "AIRFLOW_UID=$(id -u)\nAIRFLOW_GID=0" > .env
+- docker-compose up airflow-init
+- docker-compose up
+- docker exec -it docker-airflow_airflow-webserver_1 bash
+- airflow users create \
+    --username admin \
+    --firstname Peter \
+    --lastname Parker \
+    --role Admin \
+    --email spiderman@superhero.org
+
+Launch !!!!!
